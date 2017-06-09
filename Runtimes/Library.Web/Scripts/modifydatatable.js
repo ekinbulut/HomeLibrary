12e935f7d5950e29fdc194ee2e2b65ce10e2bfc5
Ekin Bulut <ekinbulut@gmail.com>
2017-06-09 23:30:48 +0300

    Bug fix, order mofied.

--- Runtimes/Library.Web/Scripts/modifydatatable.js
+++ Runtimes/Library.Web/Scripts/modifydatatable.js
@@ -2,8 +2,7 @@
     $("#example").DataTable({
         "pagingType": "full",
         responsive: true,
-        stateSave: true
-
+        "order": [[1, "asc"], [2, "asc"], [3, "asc"]]
 
     });
 });
