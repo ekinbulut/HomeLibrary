4379571b2ae80e7d6b94cf22b99689f4e07d6043
Ekin Bulut <ekinbulut@gmail.com>
2017-06-10 10:14:51 +0300

    bundle scripts and styles modified

--- Runtimes/Library.Web/App_Start/BundleConfig.cs
+++ Runtimes/Library.Web/App_Start/BundleConfig.cs
@@ -11,32 +11,69 @@ namespace Library.Web
 
             bundles.UseCdn = true;
 
-            bundles.Add(new ScriptBundle("~/bundles/jquery")
+
+            #region [ Script Bundles ]
+
+
+            // register jquery bundle from local file
+            bundles.Add(new ScriptBundle("~/bundles/js/jquery")
                 .Include("~/plugins/jQuery/jQuery-2.1.4.min.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/scripts")
-                .Include("~/bootstrap/js/bootstrap.min.js", 
-                "~/plugins/datatables/jquery.dataTables.min.js", 
-                "~/plugins/datatables/dataTables.bootstrap.min.js", 
-                "~/plugins/slimScroll/jquery.slimscroll.min.js",
-                "~/plugins/fastclick/fastclick.js", 
-                "~/dist/js/app.min.js", "~/dist/js/demo.js"));
+            // register datatable scripts from cdn
+            string uikitjs = "https://cdn.datatables.net/1.10.15/js/dataTables.uikit.min.js";
+            string datatablesjs = "https://cdn.datatables.net/1.10.15/js/jquery.dataTables.min.js";
 
-            bundles.Add(new ScriptBundle("~/bundles/datatable")
-                .Include("~/Scripts/modifydatatable.js", 
+            bundles.Add(new ScriptBundle("~/bundles/js/uikit", uikitjs));
+            bundles.Add(new ScriptBundle("~/bundles/js/datatablesjs", datatablesjs));
+
+            // register local js files
+            bundles.Add(new ScriptBundle("~/bundles/js/scripts")
+                .Include(
+                "~/bootstrap/js/bootstrap.min.js",
+                "~/plugins/slimScroll/jquery.slimscroll.min.js",
+                "~/plugins/fastclick/fastclick.js",
+                "~/dist/js/app.min.js",
+                "~/dist/js/demo.js"));
+
+            // register custom js files for database
+            bundles.Add(new ScriptBundle("~/bundles/js/datatable")
+                .Include(
+                "~/Scripts/modifydatatable.js",
                 "~/plugins/datatables/extentions/Responsive/js/dataTables.responsive.min.js"));
 
+
+
+            #endregion
+
+            #region [ Style Bundles ]
+
+            // cdn for ionic and bootstrap
             string bootstrap = "https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css";
             string ionicCdn = "https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css";
 
-            bundles.Add(new StyleBundle("~/bundles/bootstrap", bootstrap));
-            bundles.Add(new StyleBundle("~/bundles/ionic", ionicCdn));
+            // cdns for uikit - datatable
+            string uikit = "https://cdn.datatables.net/1.10.15/css/dataTables.uikit.min.css";
+            string uikitmin = "//cdnjs.cloudflare.com/ajax/libs/uikit/2.24.3/css/uikit.min.css";
 
+            // register bootstrap and ionic
+            bundles.Add(new StyleBundle("~/bundles/css/bootstrap", bootstrap));
+            bundles.Add(new StyleBundle("~/bundles/css/ionic", ionicCdn));
+
+            // register uikit
+            bundles.Add(new StyleBundle("~/bundles/css/uikit", uikit));
+            bundles.Add(new StyleBundle("~/bundles/css/uikitmin", uikitmin));
+
+            // register local css
             bundles.Add(new StyleBundle("~/bundles/css")
-                .Include("~/bootstrap/css/bootstrap.min.css",
-                "~/plugins/datatables/dataTables.bootstrap.css",
-                "~/dist/css/AdminLTE.min.css",
-                "~/dist/css/skins/_all-skins.min.css"));
+                .Include(
+                    "~/bootstrap/css/bootstrap.min.css",
+                    "~/dist/css/AdminLTE.min.css",
+                    "~/dist/css/skins/_all-skins.min.css"));
+
+
+
+            #endregion
+
 
             BundleTable.EnableOptimizations = true;
         }
