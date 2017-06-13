e2efc24cee1e7b429f7f4851ff50f2ac355fe756
Ekin Bulut <ekinbulut@gmail.com>
2017-06-13 19:22:08 +0300

    code review

--- Runtimes/Library.Web/App_Start/BundleConfig.cs
+++ Runtimes/Library.Web/App_Start/BundleConfig.cs
@@ -19,13 +19,6 @@ namespace Library.Web
             bundles.Add(new ScriptBundle("~/bundles/js/jquery")
                 .Include("~/plugins/jQuery/jQuery-2.1.4.min.js"));
 
-            // register datatable scripts from cdn
-            string uikitjs = "https://cdn.datatables.net/1.10.15/js/dataTables.uikit.min.js";
-            string datatablesjs = "https://cdn.datatables.net/1.10.15/js/jquery.dataTables.min.js";
-
-            bundles.Add(new ScriptBundle("~/bundles/js/uikit", uikitjs));
-            bundles.Add(new ScriptBundle("~/bundles/js/datatablesjs", datatablesjs));
-
             // register local js files
             bundles.Add(new ScriptBundle("~/bundles/js/scripts")
                 .Include(
@@ -35,33 +28,30 @@ namespace Library.Web
                 "~/dist/js/app.min.js",
                 "~/dist/js/demo.js"));
 
+            // register datatable scripts from cdn
+            string uikitjs = "https://cdn.datatables.net/1.10.15/js/dataTables.uikit.min.js";
+            string datatablesjs = "https://cdn.datatables.net/1.10.15/js/jquery.dataTables.min.js";
+
+            bundles.Add(new ScriptBundle("~/bundles/js/uikit", uikitjs));
+            bundles.Add(new ScriptBundle("~/bundles/js/datatablesjs", datatablesjs));
+
+
+
             // register custom js files for database
             bundles.Add(new ScriptBundle("~/bundles/js/datatable")
                 .Include(
                 "~/Scripts/modifydatatable.js",
                 "~/plugins/datatables/extentions/Responsive/js/dataTables.responsive.min.js"));
 
+            // register morris
+            string morris = "https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js";
 
+            bundles.Add(new ScriptBundle("~/bundles/js/cdn/morris",morris));
+            bundles.Add(new ScriptBundle("~/bundles/js/morris").Include("~/plugins/morris/morris.js"));
 
             #endregion
 
             #region [ Style Bundles ]
-
-            // cdn for ionic and bootstrap
-            string bootstrap = "https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css";
-            string ionicCdn = "https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css";
-
-            // cdns for uikit - datatable
-            string uikit = "https://cdn.datatables.net/1.10.15/css/dataTables.uikit.min.css";
-            string uikitmin = "//cdnjs.cloudflare.com/ajax/libs/uikit/2.24.3/css/uikit.min.css";
-
-            // register bootstrap and ionic
-            bundles.Add(new StyleBundle("~/bundles/css/bootstrap", bootstrap));
-            bundles.Add(new StyleBundle("~/bundles/css/ionic", ionicCdn));
-
-            // register uikit
-            bundles.Add(new StyleBundle("~/bundles/css/uikit", uikit));
-            bundles.Add(new StyleBundle("~/bundles/css/uikitmin", uikitmin));
 
             // register local css
             bundles.Add(new StyleBundle("~/bundles/css")
@@ -69,6 +59,24 @@ namespace Library.Web
                     "~/bootstrap/css/bootstrap.min.css",
                     "~/dist/css/AdminLTE.min.css",
                     "~/dist/css/skins/_all-skins.min.css"));
+
+            // register bootstrap and ionic
+            string bootstrap = "https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css";
+            string ionicCdn = "https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css";
+
+            bundles.Add(new StyleBundle("~/bundles/css/bootstrap", bootstrap));
+            bundles.Add(new StyleBundle("~/bundles/css/ionic", ionicCdn));
+
+            // register uikit
+            string uikit = "https://cdn.datatables.net/1.10.15/css/dataTables.uikit.min.css";
+            string uikitmin = "//cdnjs.cloudflare.com/ajax/libs/uikit/2.24.3/css/uikit.min.css";
+
+            bundles.Add(new StyleBundle("~/bundles/css/uikit", uikit));
+            bundles.Add(new StyleBundle("~/bundles/css/uikitmin", uikitmin));
+
+            // register morris
+            bundles.Add(new StyleBundle("~/bundles/css/morris")
+                .Include("~/plugins/morris/morris.css"));
 
 
 
